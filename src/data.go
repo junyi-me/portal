d7df479e7da990354e554d38518dbadfa87044f5
junyi.wang <junyi.wang.007@gmail.com>
2024-12-20 15:29:42 -0700

    external

--- src/data.go
+++ src/data.go
@@ -3,11 +3,18 @@ package main
 type Link struct {
     Text string
     URL  string
+    Favicon string
     Description string
 }
 
-var links = []Link{
-    {"profile.junyi.me", "https://profile.junyi.me/", "My personal website"},
-    {"blog.junyi.me", "https://blog.junyi.me/", "My blog"},
+var hosted = []Link{
+    {"profile.junyi.me", "https://profile.junyi.me/", "https://profile.junyi.me/favicon.png", "Personal website"},
+    {"blog.junyi.me", "https://blog.junyi.me/", "https://blog.junyi.me/favicon.ico", "My blog"},
+}
+
+var external = []Link{
+    {"github.com/jywang99", "https://github.com/jywang99/", "https://github.githubassets.com/favicons/favicon.svg", "GitHub profile"},
+    {"www.linkedin.com/in/junyi-wang-976a94199", "https://www.linkedin.com/in/junyi-wang-976a94199/", "https://static.licdn.com/aero-v1/sc/h/akt4ae504epesldzj74dzred8", "LinkedIn profile"},
+    {"junyi.wang.007@gmail.com", "mailto:junyi.wang.007@gmail.com", "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico", "Email me"},
 }
 
--- src/render.go
+++ src/render.go
@@ -11,7 +11,13 @@ func init() {
     tmpl = template.Must(template.ParseGlob("tmpl/*.html"))
 }
 
+type HomeData struct {
+    Hosted   []Link
+    External []Link
+}
+var homeData = HomeData{hosted, external}
+
 func renderHome(w http.ResponseWriter) {
-    tmpl.ExecuteTemplate(w, "home.html", links)
+    tmpl.ExecuteTemplate(w, "home.html", homeData)
 }
 
