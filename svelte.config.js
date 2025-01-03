c5541a4ece4111b88719a313952c7b9f229eb089
junyi.wang <junyi.wang.007@gmail.com>
2025-01-04 03:17:39 +0900

    pipeline

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,32 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+    // If your repo name is not equivalent to your-username.github.io, make sure to update config.kit.paths.base to match your repo name.
+    // This is because the site will be served from https://your-username.github.io/your-repo-name rather than from the root.
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
 	}
 };
 
 export default config;
+
